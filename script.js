a47a40fdd7fc841d82914c750ff4a652d16d86dc
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-03-01 12:12:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+ let form = document.querySelector('form')
+        let textbox = document.querySelector('#text')
+        let delaybox = document.querySelector('#delay')
+        let output = document.querySelector('#output')
+        let btn = document.querySelector('#btn')
+        btn.addEventListener('click', async (e)=>{
+            e.preventDefault()
+            let text = textbox.value
+            let delay = parseInt(delaybox.value)
+            await new Promise((resolve)=>{
+                setTimeout(()=>{
+                output.innerText = text
+                resolve()
+            }, delay)
+            })
+            
+        })
